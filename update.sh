d4954a9f2a39e1e49279d896362522c985da9990
flyash <fenxuehaiyan@qq.com>
2021-03-07 15:44:21 +0800

    build issue

--- update.sh
+++ update.sh
@@ -5,3 +5,12 @@ wget -O v2ray/ad.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq
 wget -O v2ray/ad-ext.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/ad-ext.hosts
 wget -O v2ray/gw.ips https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/gw.ips
 wget -O v2ray/gw.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/gw.hosts
+
+# change for smartdns
+sed 's/=/ /g' v2ray/ad.hosts > tmp
+sed 's/\/$/\/#/g' tmp > smartdns/ad.hosts
+sed 's/=/ /g' v2ray/ad-ext.hosts > tmp
+sed 's/\/$/\/#/g' tmp > smartdns/ad-ext.hosts
+sed 's/server=/nameserver /g' v2ray/gw.hosts > tmp
+sed 's/=/ /g' tmp > smartdns/gw.hosts
+rm tmp
