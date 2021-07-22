c7e06f8af25dd89fbe91507880cc2bcb1263b205
flyash <fenxuehaiyan@qq.com>
2021-07-22 10:08:16 +0800

    fix for macos & update rules

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ sed 's/=/ /g' v2ray/ad.hosts > tmp
 sed 's/\/$/\/#/g' tmp > smartdns/ad.hosts
 sed 's/=/ /g' v2ray/ad-ext.hosts > tmp
 sed 's/\/$/\/#/g' tmp > smartdns/ad-ext.hosts
-sed 's/server=/nameserver /g' v2ray/gw.hosts > tmp
-sed -i 's/127.0.0.1#1053/gw/g' tmp
-sed 's/=/ /g' tmp > smartdns/gw.hosts
-rm tmp
+sed 's/server=/nameserver /g' v2ray/gw.hosts > tmp1
+sed 's/127.0.0.1#1053/gw/g' tmp1 > tmp2
+sed 's/=/ /g' tmp2 > smartdns/gw.hosts
+rm tmp1 tmp2
