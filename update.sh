5c226e70722fcc30037e0070fe788793742f93f3
flyash <fenxuehaiyan@qq.com>
2021-12-10 17:14:20 +0800

    Update rules

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@
 mkdir tmp
 
 wget -O tmp/ad.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/ad.hosts
-wget -O tmp/ad-ext.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/ad-ext.hosts
 wget -O tmp/gw.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/gw.hosts
 
 # copy for dnsmasq
@@ -12,8 +11,6 @@ cp tmp/*.hosts dnsmasq
 # change for smartdns
 sed 's/=/ /g' tmp/ad.hosts > tmp/tmp
 sed 's/\/$/\/#/g' tmp/tmp > smartdns/ad.hosts
-sed 's/=/ /g' tmp/ad-ext.hosts > tmp/tmp
-sed 's/\/$/\/#/g' tmp/tmp > smartdns/ad-ext.hosts
 sed 's/server=/nameserver /g' tmp/gw.hosts > tmp/tmp1
 sed 's/127.0.0.1#1053/gw/g' tmp/tmp1 > tmp/tmp2
 sed 's/=/ /g' tmp/tmp2 > smartdns/gw.hosts
